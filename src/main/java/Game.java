b6d4ad67d3c5063972ead9cea4fc704dca67feee
a3a3e11o <a3a3e11o1984@gmail.com>
2023-09-02 23:31:18 +0300

    add classes

---
+++ src/main/java/Game.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JFrame {
+    private UserInterface ui;
+
+    public Game() {
+        showStartWindow();
+    }
+
+    private void showStartWindow() {
+        StartWindow startWindow = new StartWindow(this);
+        startWindow.setVisible(true);
+    }
+
+    public void startGame() {
+        initializeUI();
+        initializeGameManager();
+
+        setTitle("Міста");
+        setSize(400, 120);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void initializeUI() {
+        ui = new UserInterface();
+        add(ui, BorderLayout.CENTER);
+    }
+
+    private void initializeGameManager() {
+        GameManager gameManager = new GameManager(ui);
+    }
+}
---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class UserInterface extends JPanel {
+    private final JLabel responseLabel;
+    private final JLabel computerResponseLabel;
+    private final JTextField inputField;
+    private final JButton playButton;
+
+    public UserInterface() {
+        setLayout(new BorderLayout());
+
+        responseLabel = new JLabel("Введіть назву міста:");
+        computerResponseLabel = new JLabel("Комп'ютер: ");
+        inputField = new JTextField();
+        playButton = new JButton("Зробити хід");
+
+        responseLabel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+        computerResponseLabel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
+
+        JPanel mainPanel = new JPanel(new GridLayout(2, 1));
+        mainPanel.add(responseLabel);
+        mainPanel.add(inputField);
+        mainPanel.add(computerResponseLabel);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        playButton.setPreferredSize(new Dimension(110, 30));
+        buttonPanel.add(playButton);
+        mainPanel.add(buttonPanel);
+        add(mainPanel, BorderLayout.CENTER);
+    }
+
+    public String getUserInput() {
+        return inputField.getText().trim();
+    }
+
+    public void setResponseLabel(String text) {
+        responseLabel.setText(text);
+    }
+
+    public void setComputerResponseLabel(String text) {
+        computerResponseLabel.setText(text);
+    }
+
+    public void addActionListenerToPlayButton(ActionListener listener) {
+        playButton.addActionListener(listener);
+    }
+}
